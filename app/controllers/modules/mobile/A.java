fff0ef2f012fa226c4ea4b4cc46d5997ffbcd076
guanzheyuan <834155417@qq.com>
2017-12-05 18:15:43 +0800

    提交第三次文件

---
+++ app/controllers/modules/mobile/A.java
@@ -0,0 +1,233 @@
+package controllers.modules.mobile;
+import models.modules.mobile.WxUser;
+import models.modules.mobile.XjlDwArticle;
+import models.modules.mobile.XjlDwFile;
+import models.modules.mobile.XjlDwGroupBuy;
+import models.modules.mobile.XjlDwNotice;
+import models.modules.mobile.XjlDwNoticeFile;
+import models.modules.mobile.XjlDwWxClass;
+import net.sf.json.JSONArray;
+import play.Logger;
+import play.cache.Cache;
+import play.i18n.Messages;
+import utils.StringUtil;
+import utils.SysParamUtil;
+import utils.WxRegister;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+
+import controllers.comm.BaseController;
+import controllers.comm.SessionInfo;
+import controllers.comm.Sign;
+import controllers.modules.mobile.bo.XjlDwFileBo;
+import controllers.modules.mobile.bo.XjlDwWxClassBo;
+import controllers.modules.mobile.filter.MobileFilter;
+import controllers.modules.mobile.bo.WxUserBo;
+
+public class A extends MobileFilter {
+	 public static void manager() {
+		   render("modules/xjldw/mobile/main.html");
+	 } 
+    public static void noticeDetail() {
+    	WxUser wxUser =  getWXUser();
+		renderArgs.put("wxUser",wxUser);
+		renderArgs.put("noticeId",params.get("noticeId"));
+        render("modules/xjldw/mobile/activity/notice_detail.html");
+    }
+    
+    public static void queryNotice(){
+    	int pageIndex = StringUtil.getInteger(params.get("PAGE_INDEX"), 1);
+		int pageSize = StringUtil.getInteger(params.get("PAGE_SIZE"), 100);
+    	XjlDwNotice notice = XjlDwNotice.findById(params.get("noticeId", Long.class));
+    	List<XjlDwNoticeFile> list = (List<XjlDwNoticeFile>) XjlDwNoticeFile.queryNoticeFile(notice.noticeId, pageIndex, pageSize).get("data");
+    	notice.fileList = list;
+    	Logger.info("输出通知通告内容："+notice.noticeContent);
+        renderArgs.put("notice", notice);
+        ok(notice);
+    }
+    public static void noticeAdd() {
+    	WxUser wxUser =  getWXUser();
+		renderArgs.put("wxUser",wxUser);
+        render("modules/xjldw/mobile/activity/notice_add.html");
+    }
+    public static void noticeList() {
+    	WxUser wxUser =  getWXUser();
+		renderArgs.put("wxUser",wxUser);
+        render("modules/xjldw/mobile/activity/notice_list.html");
+    }
+    public static void articleList() {
+		WxUser wxUser =  getWXUser();
+		renderArgs.put("wxUser",wxUser);
+		if(params.get("type")!= null) {
+			renderArgs.put("type", params.get("type"));
+		}else{
+			renderArgs.put("type", "list");
+		}
+		Logger.info("----------------------------"+params.get("type")+"-----------------------");
+        render("modules/xjldw/mobile/activity/article_list.html");
+    }
+    
+    public static void queryArticleDetail(){
+    	XjlDwArticle article = XjlDwArticle.findById(Long.parseLong(params.get("articleId")));
+        ok(article);
+    }
+    public static void articleDetail() {
+    	XjlDwArticle article = XjlDwArticle.findById(params.get("articleId", Long.class));
+        renderArgs.put("detail", article);
+        renderArgs.put("articleId",params.get("articleId", Long.class));
+        renderArgs.put("type",params.get("type"));
+        render("modules/xjldw/mobile/activity/article_detail.html");
+    }
+    public static void articleAdd() {
+		XjlDwArticle article=null;
+		if(params.get("articleId")!= null) {
+			article = XjlDwArticle.findById(params.get("articleId", Long.class));
+		}else{
+			WxUser wxUser = getWXUser();
+			article=new XjlDwArticle();
+			article.articleAuthor=wxUser.nickName;
+			article.articleState="0";
+		}
+		renderArgs.put("article", article);
+        render("modules/xjldw/mobile/activity/article_add.html");
+    }
+    
+    public static void groupList() {
+		WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",wxUser);
+        render("modules/xjldw/mobile/activity/group_list.html");
+    }
+    public static void groupAdd() {
+		WxUser wxUser =  getWXUser();
+		renderArgs.put("wxUser",wxUser);
+        render("modules/xjldw/mobile/activity/group_add.html");
+    }
+    public static void groupDetail() {
+        renderArgs.put("groupBuyId", params.get("groupBuyId"));
+        renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/group_detail.html");
+     }
+    /**
+     * 相册
+     */
+    public static void albumList() {
+		WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+    	renderArgs.put("fileId", "1");
+        render("modules/xjldw/mobile/activity/album_list.html");
+    }
+    public static void albumAdd(){
+    	WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/album_add.html");
+    }
+    public static void albumImageList(){
+    	WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+		renderArgs.put("albumId", params.get("albumId"));
+//		String accessToken = Sign.getAccessToken("wx4ae50eb9b72cef71","2f66a0dd662948bc9b2b8aa26ebd0a4f");
+//		String noceStr = UUID.randomUUID().toString();
+//		String timestamp = Long.toString(System.currentTimeMillis() / 1000);
+//		String str = "jsapi_ticket="+WxRegister.getTicket(accessToken)+"&noncestr="+noceStr+"&timestamp="+timestamp+"&url=http://dw201709.com/mobile/A/albumImageList?albumId="+params.get("albumId");
+//		renderArgs.put("signature", WxRegister.SHA1(str));
+//		renderArgs.put("nonceStr", noceStr);
+//		renderArgs.put("timestamp",timestamp);
+		render("modules/xjldw/mobile/activity/album_image_list.html");
+    }
+    //选择照片
+    public static void albumAddImage(){
+    	WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/album_add_image.html");
+    }
+    //上传照片
+    public static void albumUploadImage(){
+    	WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/album_upload_image.html");
+    }
+    //上传照片成功之后的提示
+    public static void albumUploadImageOk(){
+    	WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/album_upload_image_ok.html");
+    }
+    public static void addClassAdmin(){
+   	    String userKey = session.getId()+"_userkey";
+    	WxUser wxUser =  getWXUser();
+    	wxUser=WxUser.getFindByOpenId(wxUser.wxOpenId);
+    	wxUser=WxUserBo.save(wxUser);
+    	if(wxUser!=null){
+    		XjlDwWxClass xjlDwWxClass=new XjlDwWxClass();
+    		xjlDwWxClass.classId=1l;
+    		xjlDwWxClass.isDefault="Y";
+    		xjlDwWxClass.wxOpenId=wxUser.wxOpenId;
+    		xjlDwWxClass=XjlDwWxClassBo.save(xjlDwWxClass);
+    	}
+    	wxUser = WxUser.getFindByOpenId(wxUser.wxOpenId);
+    	Cache.set(userKey,wxUser);
+    	renderArgs.put("wxUser",wxUser);
+    	render("modules/xjldw/mobile/main.html");
+    }
+    
+    //上传头像
+    public static void uploadAlbum() {
+
+        String url = params.get("url");
+        String openId = params.get("openId");
+        Logger.info("uploadopenId:"+openId);
+        Logger.info("上传图片url====" + url);
+
+        Logger.info(url);
+        String picUrl = downloadPhoto(url,openId, "A",null);
+        
+        WxUser wxUser =  getWXUser();
+        XjlDwFile xjlDwFile = XjlDwFileBo.saveImage(picUrl, wxUser.wxOpenId);
+        if(picUrl.contains(".json;")&&picUrl.endsWith("encoding=utf-8")){
+        	//上传失败
+        	nok("上传失败，公众号没有足够的权限");
+        }
+        Logger.error("==========oldFileDelete ",params.get("oldFileDelete"));
+        if (params.get("oldFileDelete")!=null) {
+        	//如果上传图片的时候指定了旧图片（或文件），则上传成功后删除原文件
+            BaseController.deleteFile(params.get("oldFileDelete"));
+		}
+        Logger.info("picUrl" + picUrl);
+        ok(xjlDwFile);
+    }
+    //上传头像
+    public static void uploadLogo() {
+
+        String url = params.get("url");
+        String openId = params.get("openId");
+        Logger.info("上传图片url====" + url);
+
+        Logger.info(url);
+        String picUrl = downloadPhoto(url,openId, "A",null);
+        WxUser wxUser =  getWXUser();
+        XjlDwFile xjlDwFile = XjlDwFileBo.saveImage(picUrl, wxUser.wxOpenId);
+        if(picUrl.contains(".json;")&&picUrl.endsWith("encoding=utf-8")){
+        	//上传失败
+        	nok("上传失败，公众号没有足够的权限");
+        }
+        Logger.error("==========oldFileDelete ",params.get("oldFileDelete"));
+        if (params.get("oldFileDelete")!=null) {
+        	//如果上传图片的时候指定了旧图片（或文件），则上传成功后删除原文件
+            BaseController.deleteFile(params.get("oldFileDelete"));
+		}
+        Logger.info("picUrl" + picUrl);
+        ok(picUrl);
+    }
+
+	
+	public static void totest(){
+		WxUser wxUser = getWXUser();
+		renderArgs.put("wxUser",getWXUser());
+        render("modules/xjldw/mobile/activity/text.html");
+	}
+}
+
---
+++ app/controllers/modules/mobile/filter/MobileFilter.java
@@ -0,0 +1,340 @@
+package controllers.modules.mobile.filter;
+
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.LoggerFactory;
+
+import models.modules.mobile.WxUser;
+import models.modules.mobile.XjlDwSchool;
+import models.modules.mobile.XjlDwWxClass;
+import models.modules.mobile.XjlDwWxStudent;
+import net.sf.json.JSONObject;
+import play.Logger;
+import play.cache.Cache;
+import play.i18n.Messages;
+import play.mvc.Before;
+import utils.CommonValidateUtil;
+import utils.DateUtil;
+import utils.HttpClientUtil;
+import utils.StringUtil;
+import controllers.LoginService;
+import controllers.comm.BaseController;
+import controllers.comm.SessionInfo;
+import controllers.comm.Sign;
+import controllers.modules.mobile.bo.WxUserBo;
+ /**
+ * @author    姓名   E-mail: 邮箱  Tel: 电话
+ * @version   创建时间：2017-3-24 下午1:47:15
+ * @describe  类说明
+ */
+public class MobileFilter extends BaseController{
+	private static org.slf4j.Logger log = LoggerFactory.getLogger(MobileFilter.class);
+	
+	private static int REQ_TIME = 0;
+	@Before(unless = { "Application.index","LoginService.index", "LoginService.mIndex","LoginService.login",
+			"LoginService.logout","LoginService.mlogout" })
+	static void checkLogin() {
+		log.debug("checkLogin开始检查登录状态");
+		String userAgent = request.headers.get("user-agent").value().toLowerCase();
+		String deviceFlag = params.get("flag");
+		String newbind = params.get("bindFlag");
+		log.debug("userAgent:"+ userAgent);
+		log.debug("deviceFlag:" + deviceFlag);
+		boolean isNeedInterface =false;
+		SessionInfo sessionInfo=getSessionInfo();
+		WxUser 	wxUser = null;
+		  if(sessionInfo!=null){
+			log.debug("当前session不为空");
+			wxUser = sessionInfo.getWxUser();
+			if(CommonValidateUtil.isMobile(userAgent)){ //手机端获取微信用户信息
+				log.debug("当前访问是通过手机访问");
+				if(wxUser!=null&&(DateUtil.getNowDate().compareTo(DateUtil.getDateTimeNowFun(wxUser.upOpenidTime,"d",1))<=0)){
+					log.debug("当前用户:" + wxUser.nickName+","+wxUser.wxOpenId);
+					//还在有效期内
+					log.debug("还在有效期内");
+					log.debug("读取信息start");
+					Logger.info("filterDate："+DateUtil.getDateTimeNowFun(wxUser.upOpenidTime,"d",1));
+					isNeedInterface = false;
+					 wxUser = WxUser.getFindByOpenId(wxUser.wxOpenId);
+					 Logger.info("toStudent:"+wxUser.isTeacher);
+					 sessionInfo.setWxUser(wxUser);
+			 		 Cache.add(MobileFilter.getSessionKey(), sessionInfo);
+			 		log.debug("读取信息end");
+				}else{
+					log.debug("已经过期，需要重新获取");
+					//已经是三天前的数据，过期重新获取
+					isNeedInterface = true;
+				}
+			}
+		}else{
+			log.debug("当前session为空");
+			if(!isMobile(userAgent)){ //pc
+				log.debug("当前浏览器不是手机，这里认为是pc");
+				if("testPC".equals(deviceFlag)){
+					log.debug("是pc上的测试标示testPC");
+					wxUser = WxUser.getFindByOpenId(null);
+					if(null != wxUser){
+						log.debug("使用模拟用户加入到session中，昵称是:" + wxUser.nickName);
+						sessionInfo=new SessionInfo();
+		        		sessionInfo.setWxUser(wxUser);
+		        		setSessionInfo(sessionInfo);
+						isNeedInterface = false;
+					}else{
+						log.error("数据库中一个微信用户都没有,模拟用户失败");
+						Logger.error("++++++++++++++++模拟oxh64jkHZeWtbUYc2AMqDc0HiJZg登录失败");
+					}
+				}else{
+					LoginService.index();
+				}
+			}else{
+				sessionInfo=new SessionInfo();
+				isNeedInterface=true;
+			}
+		}
+//		if(!isMobile(userAgent)){
+//			wxUser = WxUser.getFindByOpenId(null);
+//			if(null != wxUser){
+//				//Cache.set(userKey, wxUser);
+//        		sessionInfo.setWxUser(wxUser);
+//        		setSessionInfo(sessionInfo);
+//				isNeedInterface = false;
+//			}else{
+//				Logger.error("++++++++++++++++模拟oxh64jkHZeWtbUYc2AMqDc0HiJZg登录失败");
+//			}
+//		}
+		log.debug("isNeedInterface状态:"+isNeedInterface);
+		if(isNeedInterface){
+			String schoolKey = session.getId()+"_"+request.domain;
+			XjlDwSchool xjlDwSchool;
+			if(Cache.get(schoolKey)==null){
+				String schoolDoMain = request.domain;
+				if("localhost".equals(schoolDoMain)){
+					schoolDoMain="dw201709.com/zz";
+				}
+				xjlDwSchool = XjlDwSchool.getSchoolBySchoolId(null,schoolDoMain);
+				if(xjlDwSchool!=null){
+					Cache.set(schoolKey,xjlDwSchool);
+				}else{
+					render("modules/xjldw/mobile/no_find_school.html");
+				}
+			}else{
+				xjlDwSchool = (XjlDwSchool)Cache.get(schoolKey);
+			}
+			String appId = xjlDwSchool.wxServer.appId;
+			String secret = xjlDwSchool.wxServer.appSecret;
+			params.put("appId", appId);
+			params.put("secret", secret);
+			Logger.info("得到开发者appId:"+appId);
+			Logger.info("得到开发者secret:"+secret);
+			if(!StringUtil.isNotEmpty(appId)||!StringUtil.isNotEmpty(secret)){
+	    		nok(Messages.get("paramsLose"));
+	    	}
+			//应用授权作用域scope
+			//snsapi_base:不弹出授权页面，直接跳转，只能获取用户openid
+			//snsapi_userinfo：弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息
+			String scope = "snsapi_base"; 
+			String openId = params.get("openId");
+			String code = params.get("code");
+			//URL示例(不带参数)：http://www.baidu.com/wechat/user/index
+			String url ="http://" + request.domain + request.url;
+			//String url ="http://" + request.domain + StringUtils.substringBefore(request.url,"?");
+			//String url=URLEncoder.encode("http://xjldw.airclub.xin/mobile/m/noticeAdd");
+			Logger.info("----------------url:"+url);
+			if (code == null) {
+				Logger.info("===========redirectURL=========== " + code);
+				String redirectURL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
+						+ appId
+						+ "&redirect_uri="
+						+ url
+						+ "&response_type=code&scope="+scope+"&state=1"
+						+ "#wechat_redirect";
+				redirect(redirectURL);
+			} else {
+				Logger.info("===========code=========== " + code);
+				//通过code 调用网页授权认证得到openid
+				url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="
+			            + appId + "&secret=" + secret + "&code=" + code
+			            + "&grant_type=authorization_code";
+				JSONObject json = HttpClientUtil.invoke(url, "POST", null);
+				Logger.info("返回openidjson数据 = " + json);
+				String subscribe = "0"; //是否关注了，0：未关注，1：已关注
+				if (json!=null&&json.containsKey("openid")) {
+			        openId = json.getString("openid");
+			        subscribe = json.containsKey("subscribe")?json.getString("subscribe"):subscribe;
+			        params.remove("code");// 一次性，用完作废
+					params.put("openId", openId);
+					params.put("accessToken",json.getString("access_token"));
+					wxUser = WxUser.findById(openId);
+					//最多三天取一次
+					if(wxUser!=null&&(DateUtil.getNowDate().compareTo(DateUtil.getDateTimeNowFun(wxUser.upOpenidTime,"d",1))<=0)){
+						wxUser = WxUser.getFindByOpenId(openId);
+						sessionInfo.setWxUser(wxUser);
+		        		setSessionInfo(sessionInfo);
+					}else{
+						if(wxUser==null){
+							wxUser = new WxUser();
+							wxUser.wxOpenId = openId;
+						}
+				        String accessToken = null;
+						if("snsapi_userinfo".equals(scope)){
+					        Logger.info("通过****网页授权****获取用户基本信息");
+					        //通过code换取的是一个特殊的网页授权access_token,与基础支持中的access_token（该access_token用于调用其他接口）不同
+					        accessToken = json.getString("access_token");
+				        	url = "https://api.weixin.qq.com/sns/userinfo?access_token="+accessToken+"&openid="+openId+"&lang=zh_CN ";
+				        	json = HttpClientUtil.invoke(url, "POST", null);
+				        	Logger.info("snsapi_userinfo json = " + json);
+				        	if(json!=null&&json.containsKey("openid")&&json.containsKey("nickname")){
+				        		wxUser.nickName = json.getString("nickname");
+				        		wxUser.sex = json.getString("sex");
+				        		wxUser.sex = "1".equals(wxUser.sex)?"男":"女";
+				        		wxUser.language = json.getString("language");
+				        		wxUser.city = json.getString("city");
+				        		wxUser.province = json.getString("province"); 
+				        		wxUser.country = json.getString("country");
+				        		wxUser.headImgUrl = getWXSmallHeadImage(json.getString("headimgurl"));
+				        		wxUser.schoolId =xjlDwSchool.schoolId;
+				        		wxUser.isConcerned = "Y";
+				    			//wxUser.wxRole="0";
+				    			wxUser.openIdChanncel="web_grant";
+				    			wxUser.upOpenidTime= DateUtil.getNowDate();
+				        		wxUser = WxUserBo.save(wxUser);
+				        		wxUser = WxUser.getFindByOpenId(openId);
+				        		sessionInfo.setWxUser(wxUser);
+				        		setSessionInfo(sessionInfo);
+								//Cache.set(userKey,wxUser);
+				        	}
+						}else if("snsapi_base".equals(scope)){
+					    	Logger.info("通过****获取用户基本信息接口****获取用户信息");
+					    	accessToken = Sign.getAccessToken(appId,secret,false);
+					    	url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token="+accessToken+"&openid="+openId+"&lang=zh_CN";
+					    	json = HttpClientUtil.invoke(url, "POST", null);
+					    	if(json!=null){
+					    		Logger.info("start snsapi_base json >>>>>>>>>>>>>>> " + json);
+					    		//判断返回json是否异常
+					    		if(json.containsKey("errcode")){
+					    			accessToken = Sign.getAccessToken(appId,secret,true);
+					    			json = HttpClientUtil.invoke(url, "POST", null);
+					    			REQ_TIME++;
+					    			Logger.info("异常报错,进入重新请求计次" +REQ_TIME);
+					    		}
+					    		Logger.info(" success snsapi_base json >>>>>>>>>>>>>>>>>>>" + json);
+						    	Logger.info(" wxUser.wxOpenId==== = " + wxUser.wxOpenId);
+					    		if(json!=null&&json.containsKey("openid")){
+						    		if(json.containsKey("nickname")){
+						    			wxUser.nickName = json.getString("nickname");
+						    			wxUser.sex = json.getString("sex");
+						    			wxUser.sex = "1".equals(wxUser.sex)?"男":"女";
+						        		wxUser.language = json.getString("language");
+						    			wxUser.city = json.getString("city");
+						    			wxUser.province = json.getString("province"); 
+						    			wxUser.country = json.getString("country");
+						    			
+						    			wxUser.headImgUrl = getWXSmallHeadImage(json.getString("headimgurl"));
+						    			wxUser.schoolId =xjlDwSchool.schoolId;
+						    			wxUser.isConcerned = "Y";
+						    			//wxUser.wxRole="0";
+						    			wxUser.openIdChanncel="web_grant";
+						    			wxUser.upOpenidTime=DateUtil.getNowDate();
+						    			wxUser = WxUserBo.save(wxUser);
+						        		wxUser = WxUser.getFindByOpenId(openId);
+						        		sessionInfo.setWxUser(wxUser);
+						        		setSessionInfo(sessionInfo);
+						    		}
+						    	}
+					    	}
+						}
+					}
+			    }else{
+			    	Logger.info("...获取openid失败，请重试重试");
+				}
+				Logger.info("openid = " + openId);
+			}
+		}
+		//检查有没有绑定学生，如果没有绑定学生需要跳转到绑定学生页面
+		//wxUser = getWXUser();
+		if (wxUser!=null&&!wxUser.isTeacher&&wxUser.currentStudent == null&&("testPC".equals(deviceFlag)||isMobile(userAgent))){ //本地调试手机版页面使用
+			render("modules/xjldw/mobile/my/student_none.html");
+		} else {
+			if(!"PC".equals(sessionInfo.getDeviceFlag())){
+				log.debug("当前登录设备不是pc，是:" + sessionInfo.getDeviceFlag());
+				log.debug("当前登录用户:"+wxUser.nickName);
+				 //Logger.info("当前登录用户:"+wxUser.nickName + " 当前班级:"+wxUser.currentClass.className);	
+			}
+		}
+	}
+	/**
+	 * 得到微信头像，使用小图
+	 * @param headImageUrl
+	 * @return
+	 */
+	private static String getWXSmallHeadImage(String headImageUrl){
+		if (headImageUrl.endsWith("/0")){
+			return headImageUrl.substring(0, headImageUrl.length()-2)+"/132";
+		} else {
+			return headImageUrl;
+		}
+	}
+	private static boolean isMobile(String userAgent) {
+		if (userAgent != null) {
+			if (userAgent.indexOf("micromessenger") >= 0) {
+				return true;
+			} else if (userAgent.indexOf("pad") >= 0) {
+				return true;
+			} else if (userAgent.indexOf("android") >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * 得到微信用户，同时从缓存中获取微信登录用户信息
+	 * @return
+	 */
+	public static SessionInfo getSessionInfo() {
+		String userKey = getSessionKey();
+		log.debug("userKey:"+userKey);
+		SessionInfo sessionInfo = null;
+		if (Cache.get(userKey)!=null) {
+			sessionInfo = (SessionInfo) Cache.get(userKey);
+		}else if(sessionInfo == null){
+			log.debug("getSession > session为空");
+		}
+		return sessionInfo;
+	}
+	public static WxUser getWXUser() {
+		SessionInfo sessionInfo = getSessionInfo();
+		if (sessionInfo!=null) {
+			WxUser wxUser=sessionInfo.getWxUser();
+			log.debug("wxUser信息 openId:"+wxUser.wxOpenId + " nickName:"+wxUser.nickName);
+			return wxUser;
+		}
+		else{
+			log.debug("session为空");
+        	nok(Messages.get("appletSessionBeOverdue"));
+		}
+		return null;
+	}
+
+	private static void setSessionInfo(SessionInfo sessionInfo){
+		Cache.set(getSessionKey(), sessionInfo);
+	}
+	public static String getSessionKey(){
+		return session.getId()+"_userkey";
+	}
+	public static void main(String[] args) {
+		System.out.println(MobileFilter.getWXSmallHeadImage("http://wx.qlogo.cn/mmopen/0wRpPfN90ibAwzs8Tsvm1T9dia4kdMEWIHqCsYR3IomWSSVtCPvXHk0gSMsLibypxRmuXEA1HROlVWZUa3vE031bU1dBs26cyKT/0"));
+	}
+	
+	public static void refresh(String openid){
+		Logger.info("进入刷新方法:"+openid);
+		WxUser wxUser = WxUser.getFindByOpenId(openid);
+		SessionInfo sessionInfo=new SessionInfo();
+		sessionInfo.setWxUser(wxUser);
+		setSessionInfo(sessionInfo);
+	}
+}
+
+ 
